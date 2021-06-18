dc9c6450d6994ec7d22decb4123620bed5a668b6
abhijithtekz <84716642+abhijithtekz@users.noreply.github.com>
2021-06-19 01:17:51 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -19,7 +19,27 @@ int max (int arr[], int n)  {
   int max = arr[n-1];  
   return max;
 }
+int factors(int n, int arr[],int x)
 
+{
+int prime=0;
+for(int i=2;i<=n;i++)
+{
+if(n%i==0)
+{
+prime=1;
+for(int j=2;j<=i/2;j++)
+{
+if(i%j==0)
+{
+prime=0;
+break;
+}}
+if(prime==1)
+{int count=sizeof(arr)/sizeof(arr[i]);
+return i,count;
+}
+}}}
 int min (int arr[], int n)  {
 
   for (int i=0;i<n-1;i++)  {
