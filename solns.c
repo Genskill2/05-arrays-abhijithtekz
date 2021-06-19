500aa59a531394362f31ec1d457deb6dee7649fb
abhijithtekz <84716642+abhijithtekz@users.noreply.github.com>
2021-06-19 12:07:03 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,69 +1,64 @@
 #include <stdio.h>
-int max(int a[], int n)
+int max(int array[],int n)
 {
-    int maxn = 0;
-    for(int i = 0; i<n; i++)
-    {
-        if(a[i]>maxn)
-            maxn = a[i];
-    }
-    return maxn;
+	int max=array[0];
+	for(int i=0;i<n;i++)
+	{	if(array[i]>max)
+		max=array[i];
+	}
+	return max;
 }
-
-int min(int a[], int n)
+int min(int array[],int n)
 {
-    int minn = 2147483647;
-    for(int i = 0; i<n; i++)
-    {
-        if (a[i]< minn)
-            minn = a[i];
-    }
-    return minn;
+	int min=array[0];
+	for(int i=0;i<n;i++)
+	{	
+		if(array[i]<min)
+		min=array[i];
+	}
+	return min;
 }
-
-float average(int a[], int n)
+float average(int array[], int n)
 {
-    int sum = 0;
-    for(int i=0; i<n; i++)
-    {
-        sum+=a[i];
-    }
-    int avgn = sum/n;
-    return avgn;
+	int sum=0;		
+	for(int i=0;i<n;i++)
+	{	
+		sum+=array[i];
+	}
+	sum=sum/n;		
+	return sum;
+}	
+int mode(int array[], int n)
+{
+	int maxcount=0,maxvalue=0;
+	for(int i=0;i<n;i++)
+	{
+		int count=0;
+		for(int j=0;j<n;j++)
+		{
+			if(array[i]==array[j])
+				++count;
+		}
+		if(count>maxcount)
+		{
+			maxcount=count;
+			maxvalue=array[i];
+		}
+	return maxvalue;	
+	}
 }
-
-int mode(int a[],int n) 
+int factors(int n, int array[])
 {
-   int mxV = 0, mxC = 0;
-   for (int i = 0; i < n; ++i) 
-   {
-      int ct = 0;
-      for (int j = 0; j < n; ++j) 
-      {
-         if (a[j] == a[i])
-            ++ct;
-      }  
-      if (ct > mxC)
-      {  
-         mxC = ct;
-         mxV = a[i];
-      }
-   }
-   return mxV; 
-}
-
-int factors(int n, int a[])
-{
-    int p=n/2,j=0;
-    for(int i=2;i<p+1;)
-    {
-        if(n%i==0)
-        {
-            a[j]=i;
-            j++;
-            n=n/i;
-        }
-     else 
-     i++;
-  }return j;
+	int j=0;
+	for(int i=2;i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			array[j]=i;
+			n=n/i;
+			j++;
+			i=1;
+		}
+	}
+	return j;
 }
