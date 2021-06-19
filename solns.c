8d4d190b2af9efcbe51019f91009e263751e1ef7
abhijithtekz <84716642+abhijithtekz@users.noreply.github.com>
2021-06-19 12:36:39 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,66 +1,82 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
-#include <stdio.h>
-int max(int array[],int n)
+#include <math.h>
+
+int max(int arr[],int n)
 {
-	int max=array[0];
-	for(int i=0;i<n;i++)
-	{	if(array[i]>max)
-		max=array[i];
-	}
-	return max;
+for(int i=1;i<n;++i)
+{
+if(arr[0]<arr[i]){
+arr[0]=arr[i];
 }
-int min(int array[],int n)
-{
-	int min=array[0];
-	for(int i=0;i<n;i++)
-	{	
-		if(array[i]<min)
-		min=array[i];
-	}
-	return min;
 }
-float average(int array[], int n)
-{
-	int sum=0;		
-	for(int i=0;i<n;i++)
-	{	
-		sum+=array[i];
-	}
-	sum=sum/n;		
-	return sum;
-}	
-int mode(int array[], int n)
-{
-	int maxcount=0,maxvalue=0;
-	for(int i=0;i<n;i++)
-	{
-		int count=0;
-		for(int j=0;j<n;j++)
-		{
-			if(array[i]==array[j])
-				++count;
-		}
-		if(count>maxcount)
-		{
-			maxcount=count;
-			maxvalue=array[i];
-		}
-	return maxvalue;	
-	}
+return arr[0];
 }
-int factors(int n, int array[])
+
+int min(int arr[],int n)
 {
-	int j=0;
-	for(int i=2;i<=n;i++)
-	{
-		if(n%i==0)
-		{
-			array[j]=i;
-			n=n/i;
-			j++;
-			i=1;
+for(int i=1;i<n;i++)
+{
+if(arr[i]<arr[0]){
+arr[0]=arr[i];
+}
+}
+return arr[0];
+}
+
+float average(int arr[], int n)
+{
+float sum=0.0;
+ for(int i=0;i<n;++i){
+ sum=sum+arr[i];
+ }
+float average=sum/n;
+ 
+ return average;
+ }
+ 
+ int mode(int arr[], int n){
+int maxcount=0;
+for(int i=0;i<n;++i){
+int counts=0;
+
+for(int j=0;j<n;++j){
+if(arr[j]==arr[i])
+++counts;
+}
+if(counts>maxcount){
+maxcount=counts;
+}
+return arr[i];
+}
+}
+
+
+int factors(int num,int arr[]){
+	int prime=2,ref=0;
+	int count=0,index=0;
+	while(num!=1){
+		if(num%prime==0){
+			num=num/prime;
+			count++;
+			arr[index]=prime;
+			index++;
+		}
+		else{
+			while(1){
+				prime++;
+				for(int j=2;j<prime;j++){
+					if(prime%j==0)
+						ref++;
+				}
+				if(ref==0){
+					break;
+				}
+				else{
+					ref=0;
+				}
+			}
 		}
 	}
-	return j;
+	return count;
 }
