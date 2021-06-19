d0fe5478066a8baf764ba9927bd14533322c8234
abhijithtekz <84716642+abhijithtekz@users.noreply.github.com>
2021-06-19 13:34:30 +0530

    Update test_mode.c

--- test_mode.c
+++ test_mode.c
@@ -12,18 +12,3 @@ int main(void) {
   assert (mode(y, 1) == 5);
   printf("Mode: passed\n");
   }
-int mode(int arr[], int n){
-int maxcount=0;
-for(int i=0;i<n;++i){
-int counts=0;
-
-for(int j=0;j<n;++j){
-if(arr[j]==arr[i])
-++counts;
-}
-if(counts>maxcount){
-maxcount=counts;
-}
-return arr[i];
-}
-}
