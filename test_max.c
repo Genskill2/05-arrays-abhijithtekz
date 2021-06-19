2e32b9e5ba66fa58946c319727cc77792e687aed
abhijithtekz <84716642+abhijithtekz@users.noreply.github.com>
2021-06-19 13:32:45 +0530

    Update test_max.c

--- test_max.c
+++ test_max.c
@@ -12,12 +12,3 @@ int main(void) {
   assert (max(y, 1) == 5);
   printf("Max: passed\n");
   }
-int max(int arr[],int n)
-{
-for(int i=1;i<n;++i)
-{
-if(arr[0]<arr[i]){
-arr[0]=arr[i];
-}
-}
-return arr[0];
